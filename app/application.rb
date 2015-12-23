692df654fa4e46e393c9ddf8bf0fb7c6ff1f4159
maiya777 <maiya777@list.ru>
2015-12-23 19:58:58 +0300

    Update application.rb

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,7 @@
 class Application
-  def call(env)
-    if env["REQUEST_PATH"] == "/about"
-      response = "<h1><a href='/'>Are you lost?</a></h1>"
-    else
-      response = ""
-    end
-    [200, {"Content-Type" => "text/html"}, [response]]
+ def call(env)
+   
+    [200, {"Content-Type" => "text/html"}, [""]]
   end
 
 end
